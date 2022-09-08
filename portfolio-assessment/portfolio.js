6c14d9f4aa703349db5104c557b5983ec5c65852
SANJAY P M <pmsanjay2003@gmail.com>
2022-09-08 21:24:33 +0530

    Played with javascript code and sematic tags
    It has been long gap for update . Due to illness

--- portfolio-assessment/portfolio.js
+++ portfolio-assessment/portfolio.js
@@ -1,7 +1,10 @@
 function show(name){
-    var content;
-    content=document.getElementsByClassName('tab-content');
+    var content=document.getElementsByClassName('tab-content');
+    var tabs = document.getElementsByClassName('tab-link');
     for(var i=0;i<content.length;i++)
-        content[i].style.display='none';
-    document.getElementById(name).style.display='block';
+        content[i].classList.remove('active-content');
+    document.getElementById(name).classList.add('active-content');
+    for(var i=0;i<content.length;i++)
+        tabs[i].classList.remove("active-link");
+    event.currentTarget.classList.add("active-link")
 }
